af1ca4b43e4d2a91bccb4d50dff56f294a3e4945
Jesús Alberto Mora <jesusmora0101@gmail.com>
2021-10-11 17:44:41 -0500

    Initial Commit

---
+++ HackerNewsApp/HackerNewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackerNewsAppApp.swift
+//  HackerNewsApp
+//
+//  Created by Jesus Mora on 11/10/21.
+//
+
+import SwiftUI
+
+@main
+struct HackerNewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
